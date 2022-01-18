a75788c6a83bb6236dae5c0bcba7127fe0053dfa
Mike543 <44565608+Mike543@users.noreply.github.com>
2022-01-18 18:47:07 +0300

    add lesson_06

--- log/client_log_config.py
+++ log/client_log_config.py
@@ -1,13 +1,27 @@
-
+import os
 import logging
-import log
+from datetime import datetime
 
-logging.basicConfig(
-    filename='client.log',
-    format='%(asctime)-10s %(levelname)s %(module)s %(message)s',
-    level=logging.INFO
-)
+from lesson_06.utils.variables import LOGGING_LEVEL
 
-# log = logging.getLogger('basic')
-#
-# log.warning('Warning')
+LOG = logging.getLogger('client')
+
+FORMATTER = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(message)s')
+
+LOG_PATH = os.path.dirname(os.path.abspath(__file__))
+LOG_PATH = os.path.join(LOG_PATH, 'logs/client_log_files/client.log')
+
+LOG_FILE = logging.FileHandler(LOG_PATH, encoding='utf8')
+LOG_FILE.setFormatter(FORMATTER)
+
+LOG.addHandler(LOG_FILE)
+LOG.setLevel(LOGGING_LEVEL)
+
+LOG.info(f'Лог-файл от {datetime.today()}')
+
+# отладка
+if __name__ == '__main__':
+    LOG.critical('ОТЛАДКА Критическая ошибка')
+    LOG.error('ОТЛАДКА Ошибка')
+    LOG.debug('ОТЛАДКА Отладочная информация')
+    LOG.info('ОТЛАДКА Информационное сообщение')
